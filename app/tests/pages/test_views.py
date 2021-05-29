b58c8cfc458c0180495b3fdb9819c4d9e347437d
julianpetrich <julian@julianpetrich.com>
2021-05-29 17:14:09 +0200

    fixes pytests

--- app/tests/pages/test_views.py
+++ app/tests/pages/test_views.py
@@ -21,3 +21,31 @@ def test_home_context(client, create_post):
     create_post()
     response = client.get(reverse("home"))
     assert "post_list" in response.context
+
+
+@pytest.mark.django_db
+def test_about_view(client):
+    url = reverse("about")
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_contact_view(client):
+    url = reverse("contact")
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_term_view(client):
+    url = reverse("terms")
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_privacy_view(client):
+    url = reverse("privacy")
+    response = client.get(url)
+    assert response.status_code == 200
